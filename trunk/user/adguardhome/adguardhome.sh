60ba4e6ddb74fb8110bd81036b5df42ce0e9319f
leonzhao <ask870@163.com>
2020-08-05 00:24:56 +0800

    Add files via upload

--- trunk/user/adguardhome/adguardhome.sh
+++ trunk/user/adguardhome/adguardhome.sh
@@ -144,14 +144,14 @@ fi
 dl_adg(){
 logger -t "AdGuardHome" "下载AdGuardHome"
 #wget --no-check-certificate -O /tmp/AdGuardHome.tar.gz https://github.com/AdguardTeam/AdGuardHome/releases/download/v0.101.0/AdGuardHome_linux_mipsle.tar.gz
-#curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 10 --retry 3 https://cdn.jsdelivr.net/gh/chongshengB/rt-n56u/trunk/user/adguardhome/AdGuardHome
+curl -k -s -o /tmp/AdGuardHome/AdGuardHome --connect-timeout 100 --retry 2 https://hk.leonzhao.top/AdGuardHome
 mkdir -p /tmp/AdGuardHome
 if [ ! -f "/tmp/AdGuardHome/AdGuardHome" ]; then
-#logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
-#nvram set adg_enable=0
-#exit 0
-#else
-#logger -t "AdGuardHome" "AdGuardHome下载成功。"
+  logger -t "AdGuardHome" "AdGuardHome下载失败，请检查是否能正常访问github!程序将退出。"
+  nvram set adg_enable=0
+  exit 0
+else
+  logger -t "AdGuardHome" "AdGuardHome下载成功。"
   chmod 777 /tmp/AdGuardHome/AdGuardHome
 fi
 }
@@ -170,7 +170,7 @@ start_adg(){
 
 }
 stop_adg(){
-#rm -rf /tmp/AdGuardHome
+rm -rf /tmp/AdGuardHome
 rm -rf /tmp/AdGuardHome/data
 killall -9 AdGuardHome
 del_dns
